fc576646b3478109ec27629c876e254f2bceed24
Bravocado <bravocado.project@gmail.com>
2015-05-23 03:39:15 +0700

    fadein the content

--- build/scripts/init.js
+++ build/scripts/init.js
@@ -1,6 +1,89 @@
-/*============================================
-=            FUNCTION CHECK CLASS            =
-============================================*/
+/*===========================================
+=            DOCREADY JAVASCRIPT            =
+===========================================*/
+
+(function(funcName, baseObj) {
+	"use strict";
+	// The public function name defaults to window.docReady
+	// but you can modify the last line of this function to pass in a different object or method name
+	// if you want to put them in a different namespace and those will be used instead of 
+	// window.docReady(...)
+	funcName = funcName || "ready";
+	baseObj = baseObj || window;
+	var readyList = [];
+	var readyFired = false;
+	var readyEventHandlersInstalled = false;
+	
+	// call this when the document is ready
+	// this function protects itself against being called more than once
+	function ready() {
+		if (!readyFired) {
+			// this must be set to true before we start calling callbacks
+			readyFired = true;
+			for (var i = 0; i < readyList.length; i++) {
+				// if a callback here happens to add new ready handlers,
+				// the docReady() function will see that it already fired
+				// and will schedule the callback to run right after
+				// this event loop finishes so all handlers will still execute
+				// in order and no new ones will be added to the readyList
+				// while we are processing the list
+				readyList[i].fn.call(window, readyList[i].ctx);
+			}
+			// allow any closures held by these functions to free
+			readyList = [];
+		}
+	}
+	
+	function readyStateChange() {
+		if ( document.readyState === "complete" ) {
+			ready();
+		}
+	}
+	
+	// This is the one public interface
+	// docReady(fn, context);
+	// the context argument is optional - if present, it will be passed
+	// as an argument to the callback
+	baseObj[funcName] = function(callback, context) {
+		// if ready has already fired, then just schedule the callback
+		// to fire asynchronously, but right away
+		if (readyFired) {
+			setTimeout(function() {callback(context);}, 1);
+			return;
+		} else {
+			// add the function and context to the list
+			readyList.push({fn: callback, ctx: context});
+		}
+		// if document already ready to go, schedule the ready function to run
+		// IE only safe when readyState is "complete", others safe when readyState is "interactive"
+		if (document.readyState === "complete" || (!document.attachEvent && document.readyState === "interactive")) {
+			setTimeout(ready, 1);
+		} else if (!readyEventHandlersInstalled) {
+			// otherwise if we don't have event handlers installed, install them
+			if (document.addEventListener) {
+				// first choice is DOMContentLoaded event
+				document.addEventListener("DOMContentLoaded", ready, false);
+				// backup is window load event
+				window.addEventListener("load", ready, false);
+			} else {
+				// must be IE
+				document.attachEvent("onreadystatechange", readyStateChange);
+				window.attachEvent("onload", ready);
+			}
+			readyEventHandlersInstalled = true;
+		}
+	}
+})("ready", window);
+// modify this previous line to pass in your own method name 
+// and object for the method to be attached to
+
+
+/*-----  End of DOCREADY JAVASCRIPT  ------*/
+
+
+/*=======================================
+=            EXISTS FUNCTION            =
+=======================================*/
 
 // for checking if the element exists or not
 $.fn.exists = function(callback) {
@@ -11,7 +94,7 @@ $.fn.exists = function(callback) {
 	return this;
 };
 
-/*-----  End of FUNCTION CHECK CLASS  ------*/
+/*-----  End of EXISTS FUNCTION  ------*/
 
 
 
@@ -21,11 +104,17 @@ $.fn.exists = function(callback) {
 =====================================================*/
 
 
-$(document).ready(function() {
+ready(function () {
 
-	var svgs = $('svg');
-	svgs.show();
-	
+	var content = $('.content');
+	content.fadeIn();
+
+	if (document.querySelector('.mydivclass') !== null) {
+		console.log('ada');
+	} else {
+		console.log('gak ada');
+	}
+
 	new Vivus('topline', {
 		type: 'scenario',
 		pathTimingFunction: Vivus.LINEAR
@@ -52,10 +141,6 @@ $(document).ready(function() {
 		function doDone(obj) {
 			obj.el.classList.add('finished');
 	});
-
-	var gerobakLoad = document.querySelectorAll('[gerobak-load]');
-	$(gerobakLoad).remove();
-
 
 });
 // end ready
