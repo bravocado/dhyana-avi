cb620f77a21ddef58956c0562375f88c884c2829
Bravocado <bravocado.project@gmail.com>
2015-05-27 13:32:02 +0700

    added scrollmagic. added favicon.

--- build/scripts/animator.js
+++ build/scripts/animator.js
@@ -1,2 +1,4 @@
 //@codekit-append "../../bower_components/gsap/src/uncompressed/TweenMax.js";
-//@codekit-append "../../bower_components/vivus/dist/vivus.js";
+//@codekit-append "../../bower_components/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js";
+//@codekit-append "../../bower_components/scrollmagic/scrollmagic/uncompressed/plugins/jquery.ScrollMagic.js";
+//@codekit-append "../../bower_components/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js";
--- build/scripts/init.js
+++ build/scripts/init.js
@@ -1,3 +1,4 @@
+
 /*=======================================
 =            EXISTS FUNCTION            =
 =======================================*/
@@ -20,23 +21,18 @@ $.fn.exists = function(callback) {
 =            DOCUMENT READY FIRE FUNCTIONS            =
 =====================================================*/
 
-
 $(document).ready(function() {
 
 	var content = $('.content');
 	content.fadeIn();
 
-
 	/*==========  ANIMATOR  ==========*/
-	
+
+
 	var
-		titleContainer = $('.titleContainer')
+		vh = window.innerHeight
+		,titleContainer = $('.titleContainer')
 		,titlePath = $('.titleContainer path')
-		,contentAnimator = $('#contentAnimation > .animation')
-		,placeAnimator = $('#placeAnimation > .animation')
-		,titleTimline = new TimelineMax()
-		,contentTimeline = new TimelineMax({paused:true})
-		,placeTimeline = new TimelineMax({paused:true})
 	;
 
 	titlePath.each(function(i, e) {
@@ -47,97 +43,169 @@ $(document).ready(function() {
 		console.log('callback added!');
 	}
 
-	function titleAnimation() {
-		titleTimline.add([
-			TweenLite.to(titleContainer, 1,{top:150,ease:Back.easeOut}),
-			TweenLite.to(titlePath.eq(0), 2, {
-				strokeDashoffset: 0,
-				delay: 0,
-				fillOpacity: 1,
-			}),
-			TweenLite.to(titlePath.eq(1), 2, {
-				strokeDashoffset: 0,
-				delay: 0,
-				fillOpacity: 1,
-			}),
-			TweenLite.to(titlePath.eq(2), 2, {
-				strokeDashoffset: 0,
-				delay: 0,
-				fillOpacity: 1,
-			}),
-			// curve line
-			TweenLite.to(titlePath.eq(3), 2, {
-				strokeDashoffset: 0,
-				delay: 2,
-				fillOpacity: 1,
-			}),
-			TweenLite.to(titlePath.eq(4), 2, {
-				strokeDashoffset: 0,
-				delay: 2.2,
-			}),
-			TweenLite.to(titlePath.eq(5), 2, {
-				strokeDashoffset: 0,
-				delay: 2,
-			}),
-			TweenLite.to(titlePath.eq(6), 2, {
-				strokeDashoffset: 0,
-				delay: 2.2,
-				fillOpacity: 1,
-			}),
-			// dhyana&avi
-			TweenLite.to([titlePath.eq(7), titlePath.eq(8), titlePath.eq(8), titlePath.eq(9), titlePath.eq(10), titlePath.eq(11), titlePath.eq(12), titlePath.eq(13), titlePath.eq(14), titlePath.eq(15), titlePath.eq(16)], 2, {
-				strokeDashoffset: 0,
-				delay: 2.4,
-				fillOpacity: 1,
-			}),
-			// bottomdecor
-			TweenLite.to(titlePath.eq(24), 2, {
-				strokeDashoffset: 0,
-				delay: 2.8,
-				fillOpacity: 1,
-			}),
-			TweenLite.to(titlePath.eq(25), 2, {
-				strokeDashoffset: 0,
-				delay: 2.8,
-				fillOpacity: 1,
-			}),
-			// jakarta
-			TweenLite.to([titlePath.eq(17),titlePath.eq(18),titlePath.eq(19),titlePath.eq(20),titlePath.eq(21),titlePath.eq(22),titlePath.eq(23)], 2, {
-				strokeDashoffset: 0,
-				delay: 2.4,
-				fillOpacity: 1,
-			}),
-			//left and right decor at bottom
-			TweenLite.to([titlePath.eq(26),titlePath.eq(27),titlePath.eq(28),titlePath.eq(29),titlePath.eq(30),titlePath.eq(31)], 2, {
-				strokeDashoffset: 0,
-				delay: 3,
-				fillOpacity: 1,
-			}),
-		]);
-	}
-
 	// var tn1 to use stagger mode
 	//var tn1 = new TweenMax.staggerFrom(contentAnimator, 1, {autoAlpha:0, top:100, ease:Back.easeOut, paused:true}, 1);
-	function contentAnimation() {
+	//function contentAnimation() {
 		//$.each(tn1, function(){this.play();})
-		TweenLite.to(titleContainer, 1,{top:0,ease:Back.easeOut}),
-		$.each(contentAnimator, function() {
-			contentTimeline.fromTo($(this), 1,{autoAlpha:0, top:100}, {display: 'block', autoAlpha:1, top:0, ease:Back.easeOut});
+		//TweenLite.to(titleContainer, 1,{top:0,ease:Back.easeOut}),
+	//}
+
+	//function placeAnimation() {
+		//placeTimeline.fromTo(placeAnimator, 1,{autoAlpha:0, top:100}, {display: 'block', autoAlpha:1, top:0, ease:Back.easeOut});
+		//placeTimeline.play(0);
+	//}
+
+	$('.layer').each(function(){
+		$(this).css('height', vh);
+	});
+
+	function callback (event) {
+		console.log("Event fired! (" + event.type + ")");
+	}
+
+	// init ScrollMagic Controller
+	var controller = new ScrollMagic.Controller(
+		{
+			// globalSceneOptions: {
+			// 	triggerHook: 'onLeave'
+			// }
+		}
+	);
+
+	// title animation
+	var titleAnimation = new TimelineMax();
+	titleAnimation.add([
+		TweenLite.to(titlePath.eq(0), 2, {
+			strokeDashoffset: 0,
+			delay: 0,
+			fillOpacity: 1,
+		}),
+		TweenLite.to(titlePath.eq(1), 2, {
+			strokeDashoffset: 0,
+			delay: 0,
+			fillOpacity: 1,
+		}),
+		TweenLite.to(titlePath.eq(2), 2, {
+			strokeDashoffset: 0,
+			delay: 0,
+			fillOpacity: 1,
+		}),
+		// curve line
+		TweenLite.to(titlePath.eq(3), 2, {
+			strokeDashoffset: 0,
+			delay: 2,
+			fillOpacity: 1,
+		}),
+		TweenLite.to(titlePath.eq(4), 2, {
+			strokeDashoffset: 0,
+			delay: 2.2,
+		}),
+		TweenLite.to(titlePath.eq(5), 2, {
+			strokeDashoffset: 0,
+			delay: 2,
+		}),
+		TweenLite.to(titlePath.eq(6), 2, {
+			strokeDashoffset: 0,
+			delay: 2.2,
+			fillOpacity: 1,
+		}),
+		// dhyana&avi
+		TweenLite.to([titlePath.eq(7), titlePath.eq(8), titlePath.eq(8), titlePath.eq(9), titlePath.eq(10), titlePath.eq(11), titlePath.eq(12), titlePath.eq(13), titlePath.eq(14), titlePath.eq(15), titlePath.eq(16)], 2, {
+			strokeDashoffset: 0,
+			delay: 2.4,
+			fillOpacity: 1,
+		}),
+		// bottomdecor
+		TweenLite.to(titlePath.eq(24), 2, {
+			strokeDashoffset: 0,
+			delay: 2.8,
+			fillOpacity: 1,
+		}),
+		TweenLite.to(titlePath.eq(25), 2, {
+			strokeDashoffset: 0,
+			delay: 2.8,
+			fillOpacity: 1,
+		}),
+		// jakarta
+		TweenLite.to([titlePath.eq(17),titlePath.eq(18),titlePath.eq(19),titlePath.eq(20),titlePath.eq(21),titlePath.eq(22),titlePath.eq(23)], 2, {
+			strokeDashoffset: 0,
+			delay: 2.4,
+			fillOpacity: 1,
+		}),
+		//left and right decor at bottom
+		TweenLite.to([titlePath.eq(26),titlePath.eq(27),titlePath.eq(28),titlePath.eq(29),titlePath.eq(30),titlePath.eq(31)], 2, {
+			strokeDashoffset: 0,
+			delay: 3,
+			fillOpacity: 1,
+		}),
+	]);
+
+	// content animation
+	var contentAnimation = new TimelineMax();
+	contentAnimation.add([
+		TweenLite.fromTo('.contentLeft', 1, {
+			autoAlpha:0
+		}, {
+			autoAlpha:1,
+			delay: 1,
+			ease:Back.easeOut
+		}),
+		TweenLite.fromTo('.contentCenter', 1, {
+			autoAlpha:0
+		}, {
+			autoAlpha:1,
+			delay: 1.1,
+			ease:Back.easeOut
+		}),
+		TweenLite.fromTo('.contentRight', 1, {
+			autoAlpha:0
+		}, {
+			autoAlpha:1,
+			delay: 1.2,
+			ease:Back.easeOut
+		}),
+	]);
+
+	var placeAnimation = new TimelineMax();
+	placeAnimation.add([
+		TweenLite.fromTo('#placeAnimation', 1,{
+			autoAlpha:0
+		}, {
+			autoAlpha:1,
+			delay: 0.5,
+			ease:Back.easeOut
+		})
+	]);
+
+	var scene2 = new ScrollMagic.Scene({
+		triggerHook: 0,
+		triggerElement: '#secondLayer',
+		offset: -200,
+		duration: 0
+	})
+	.addTo(controller)
+	.setTween(titleAnimation)
+	.on("enter leave", function (event) {
+			console.log("Scene change.");
+			callback;
 		});
-		contentTimeline.play(0);
-	}
+	;
 
-	function placeAnimation() {
-		placeTimeline.fromTo(placeAnimator, 1,{autoAlpha:0, top:100}, {display: 'block', autoAlpha:1, top:0, ease:Back.easeOut});
-		placeTimeline.play(0);
-	}
+	var scene3 = new ScrollMagic.Scene({
+		triggerHook: 'onEnter',
+		triggerElement: '#thirdLayer',
+		duration: 0
+	})
+	.setTween(contentAnimation)
+	.addTo(controller);
 
-
-	// run title animation
-	titleAnimation();
-	// when title animation done
-	titleTimline.eventCallback("onComplete", contentAnimation);
-	contentTimeline.eventCallback("onComplete", placeAnimation);
+	var scene4 = new ScrollMagic.Scene({
+		triggerHook: 'onEnter',
+		triggerElement: '#fourthLayer',
+		duration: 0
+	})
+	.setTween(placeAnimation)
+	.addTo(controller);
 
 
 });
